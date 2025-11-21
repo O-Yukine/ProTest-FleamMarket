f93fb01138f03952aa479fa6cbb96090b68e6253
Yukine <yukineriko@gmail.com>
2025-11-21 17:52:38 +0900

    いいね機能のテストコード

---
+++ src/tests/Feature/Like/LikeTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tests\Feature\Like;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Product;
+
+class LikeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_can_like_item()
+    {
+        $seller = User::factory()->create();
+        $user = User::factory()->create();
+
+        $product = Product::factory()->create([
+            'user_id' => $seller->id,
+        ]);
+
+        $this->actingAs($user);
+
+        $this->assertDatabaseMissing('user_product_likes', [
+            'product_id' => $product->id,
+            'user_id' => $user->id,
+        ]);
+
+        $this->post('/item/' . $product->id . '/like');
+
+        $this->assertDatabaseHas('user_product_likes', [
+            'product_id' => $product->id,
+            'user_id' => $user->id,
+        ]);
+    }
+
+    public function test_button_color_will_change()
+    {
+
+        $seller = User::factory()->create();
+        $user = User::factory()->create();
+
+        $product = Product::factory()->create([
+            'user_id' => $seller->id,
+        ]);
+        $product->likedBy()->attach($user->id);
+
+
+        $this->actingAs($user);
+        $response = $this->get('/item/' . $product->id);
+
+        $response->assertSee('images/hart_like.png');
+        $response->assertDontSee('images/hart_unlike.png');
+
+        $this->post('/item/' . $product->id . '/like');
+
+        $response = $this->get('/item/' . $product->id);
+
+        $response->assertSee('images/hart_unlike.png');
+        $response->assertDontSee('images/hart_like.png');
+    }
+
+    public function test_user_can_unlike_item()
+    {
+
+        $seller = User::factory()->create();
+        $user = User::factory()->create();
+
+        $product = Product::factory()->create([
+            'user_id' => $seller->id,
+        ]);
+
+        $product->likedBy()->attach($user->id);
+
+        $this->actingAs($user);
+
+        $this->assertDatabasehas('user_product_likes', [
+            'product_id' => $product->id,
+            'user_id' => $user->id,
+        ]);
+
+        $this->post('/item/' . $product->id . '/like');
+
+        $this->assertDatabaseMissing('user_product_likes', [
+            'product_id' => $product->id,
+            'user_id' => $user->id,
+        ]);
+    }
+}
